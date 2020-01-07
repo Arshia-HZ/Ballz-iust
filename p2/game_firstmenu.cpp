38a467841977751f3ee3c4f4441c44b8484c5a04
Ali Baqerzadeh <bagherzadeh007@gmail.com>
2020-01-08 01:18:55 +0330

    A text added to the first page . Now we have a red page a ball and a text

--- p2/game_firstmenu.cpp
+++ p2/game_firstmenu.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 using namespace std ;
 class Obj {
   public :
@@ -24,12 +25,34 @@ class Obj {
     tex = SDL_CreateTextureFromSurface(ren,surf);
   }
 };
+
+void WriteMessage(const char * msg , int x , int y , int r , int g , int b , int size,SDL_Renderer* ren) {
+  SDL_Surface* surf ;
+  SDL_Texture* tex ;
+  TTF_Font* font = TTF_OpenFont("GTA.ttf",size);
+  SDL_Color color ;
+  color.r = r ;
+  color.g = g ;
+  color.b = b ;
+  color.a = 255 ;
+  SDL_Rect rect ;
+  surf = TTF_RenderText_Solid(font,msg,color);
+  tex = SDL_CreateTextureFromSurface(ren,surf);
+  rect.x=x;
+  rect.y=y;
+  rect.w=surf->w;
+  rect.h=surf->h;
+  SDL_FreeSurface(surf);
+  SDL_RenderCopy(ren,tex,NULL,&rect);
+  SDL_DestroyTexture(tex);
+}
 int main(){
   SDL_Init(SDL_INIT_EVERYTHING);
+  TTF_Init();
   SDL_Renderer* ren ;
   SDL_Window* win ;
   SDL_CreateWindowAndRenderer(800,600,0,&win,&ren);
-  SDL_SetWindowTitle(win,"SwipeBrickBreaker");
+  SDL_SetWindowTitle(win,"Swipe Brick Breaker");
   SDL_SetRenderDrawColor(ren,255,0,0,255);
   SDL_Rect rect ;
   rect.x = 0 ;
@@ -44,6 +67,7 @@ int main(){
   while (r) {
     SDL_RenderFillRect(ren,&rect);
     SDL_RenderCopyEx(ren,ball.tex,&ball.src,&ball.dest,0,NULL,SDL_FLIP_NONE);
+    WriteMessage("Swipe Brick Breaker",160,0,0,200,150,45,ren);
     SDL_RenderPresent(ren);
     SDL_Delay(3000);
     SDL_Quit();
