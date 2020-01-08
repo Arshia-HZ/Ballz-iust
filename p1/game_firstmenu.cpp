de312618d22dea790109ffe202294552fb6edd6d
Mehdi Mostafavi <seyedmehdimostafavi79@gmail.com>
2020-01-08 09:55:02 +0330

    Changed

--- p1/game_firstmenu.cpp
+++ p1/game_firstmenu.cpp
@@ -16,7 +16,7 @@ int main( int argc, char* args[] )
 	}
 	else
 	{
-		//Create
+		//Create A WINDOW
 		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if( window == NULL )
 		{
