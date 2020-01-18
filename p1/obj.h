e14973c35d4bfc64d38c3e3c24c8c0b0c07a6cc4
AliBagherz <56173095+AliBagherz@users.noreply.github.com>
2020-01-18 14:03:50 +0330

    obj.h created

---
+++ p1/obj.h
@@ -0,0 +1,33 @@
+#ifndef OBJ_H
+#define OBJ_H
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+using namespace std ;
+
+class Obj {
+  public :
+  SDL_Rect dest ;
+  SDL_Rect src;
+  SDL_Texture* tex ;
+  void setDest(int x , int y , int w , int h) {
+    dest.x = x ;
+    dest.y = y ;
+    dest.w = w ;
+    dest.h = h ;
+  }
+  void setSource(int x , int y , int w , int h) {
+    src.x = x ;
+    src.y = y ;
+    src.w = w ;
+    src.h = h ;
+  }
+  void setImage(string filename , SDL_Renderer* ren) {
+    SDL_Surface* surf = IMG_Load(filename.c_str());
+    tex = SDL_CreateTextureFromSurface(ren,surf);
+  }
+};
+
+#endif
