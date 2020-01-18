8cf617d092adc3510e124e7aec2834ed9f534679
Mehdi Mostafavi <seyedmehdimostafavi79@gmail.com>
2020-01-18 23:52:58 +0330

    ...

--- p1/GameEngine.cpp
+++ p1/GameEngine.cpp
@@ -3,4 +3,12 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL.h>
+#include "firstmenu.cpp"
+#include "settingsmenu.cpp"
 using namespace std;
+
+
+int int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
