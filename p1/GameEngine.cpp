f04fd3cd6dd8fa9c70032879608a9823a07be218
Mehdi Mostafavi <seyedmehdimostafavi79@gmail.com>
2020-01-19 16:46:26 +0330

    GameEngine fixed

--- p1/GameEngine.cpp
+++ p1/GameEngine.cpp
@@ -5,22 +5,24 @@
 using namespace std;
 #include "headers/Game.h"
 #include "headers/Object.h"
-SDL_Window* win;
-SDL_Texture* tex;
+SDL_Window* win ;
+SDL_Texture* tex ;
 int status = 0;
 bool windowloop = true;
 #include "firstmenu.cpp"
 #include "settingsmenu.cpp"
+#include "game.cpp"
 #include "ingamemenu.cpp"
 #include "lastmenu.cpp"
 
 int main(int argc, char const *argv[]) {
-        while(windowloop) {
-Loop:           switch(status) {
-                case 0: firstmenu();  break;
-                case 1: settingsmenu(); break;
-                }
-        }
+  while(windowloop){
+  switch(status){
+    case 0: firstmenu();  break;
+    case 1: settingsmenu(); break;
+    case 2: game() ; break;
+  }
+}
 //SDL_CreateWindowAndRenderer(350,600,0,&win,&ren_fm);
-        return 0;
+  return 0;
 }
