5366844da3b3cf0e76bdd589fa8ced5c9af30086
Mehdi Mostafavi <seyedmehdimostafavi79@gmail.com>
2020-01-19 21:23:29 +0330

    added data saving feature

---
+++ p1/database.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+fstream savefile;
+//Openning file
+bool openfile(bool clean = false){
+  if(clean){
+      savefile.open("data/highscore.txt",ofstream::out | ofstream::trunc);
+  }else{
+      savefile.open("data/highscore.txt");
+  }
+  if(savefile.is_open()){
+    return true;
+  }else{
+    return false;
+  }
+}
+// Reading score from file
+int returnscore(){
+  string saved_highscore;
+  if(openfile()){
+    getline(savefile,saved_highscore);
+  }else{
+    cout<<"ERROR OPENING FILE TO READ DATA";
+    return -1;
+  }
+  savefile.close();
+  return stoi(saved_highscore);
+}
+//Saving score to file
+bool savescore(int highscore){
+  if(openfile(true)){
+    savefile << highscore <<endl;
+  }else{
+    cout<<"ERROR  OPENIN FILE FOR SAVING DATA";
+    savefile.close();
+    return false;
+  }
+  savefile.close();
+  return true;
+}
