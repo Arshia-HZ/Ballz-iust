ee0421b23fcb47bd44a1d8c2398e016dd2a152c7
Mehdi Mostafavi <seyedmehdimostafavi79@gmail.com>
2020-01-22 13:10:31 +0330

    changed gameEngine and lastmenu

--- lastmenu.cpp
+++ lastmenu.cpp
@@ -1,61 +1,85 @@
 Obj playagain_lm,highscore_lm,firstmenu_lm ;
 SDL_Renderer* ren_lm;
 SDL_Surface* surf_lm ;
+SDL_Texture* text;
+SDL_Rect dest;
+SDL_Color foreground = { 0, 0, 0 };
+TTF_Font* font;
 bool running_lm = true ;
 void input_lm() {
-  while (SDL_PollEvent(&e)) {
+	static const unsigned char* keys = SDL_GetKeyboardState( NULL );
+  while (SDL_PollEvent(&e) != 0) {
     SDL_GetMouseState(&mousex,&mousey) ;
-    if (e.type == SDL_QUIT) {
-      running_lm = false ;
+		cout<<" y";
+    switch(e.type){
+      case SDL_QUIT:
+      running_lm = false;
       windowloop = false;
       SDL_Quit();
       return;
-    }
-    if (e.type == SDL_KEYDOWN) {
+      break;
+      case SDL_KEYDOWN:
       if(e.key.keysym.sym == SDLK_ESCAPE) {
         running_lm = false;
         windowloop = false;
         SDL_Quit();
         return;
       }
-    }
-    if (e.type == SDL_MOUSEBUTTONDOWN) {
+			if (e.key.keysym.sym == SDLK_BACKSPACE && playername.size()) {
+				playername.pop_back();
+			}
+      break;
+    case SDL_MOUSEBUTTONDOWN:
       if ((mousex>=playagain_lm.dest.x)&&(mousex<=playagain_lm.dest.x+playagain_lm.dest.w)&&(mousey>=playagain_lm.dest.y)&&(mousey<=playagain_lm.dest.y+playagain_lm.dest.h)) {
         status = 2;
         SDL_DestroyRenderer(ren_lm);
         running_lm = false;
+        SDL_StopTextInput();
         return;
       }
       if ((mousex>=highscore_lm.dest.x)&&(mousex<=highscore_lm.dest.x+highscore_lm.dest.w)&&(mousey>=highscore_lm.dest.y)&&(mousey<=highscore_lm.dest.y+highscore_lm.dest.h)) {
-      //  status = 1;
-      //  SDL_DestroyRenderer(ren_lm);
-      //  running_lm = false;
-      //  return;
-      cout<<"highscore_lm clicked";
+      cout<<"Highscore clicked";
       }
       if ((mousex>=firstmenu_lm.dest.x)&&(mousex<=firstmenu_lm.dest.x+firstmenu_lm.dest.w)&&(mousey>=firstmenu_lm.dest.y)&&(mousey<=firstmenu_lm.dest.y+firstmenu_lm.dest.h)) {
         status = 0;
         SDL_DestroyRenderer(ren_lm);
         running_lm = false;
+        SDL_StopTextInput();
         return;
-      }
-    /*  if ((mousex>=ball.dest.x)&&(mousex<=ball.dest.x+ball.dest.w)&&(mousey>=ball.dest.y)&&(mousey<=ball.dest.y+ball.dest.h)) {
-        cout << "ball Clicked" << endl ;
-        // Open The Balls Page
-      }
-      if ((mousex>=high.dest.x)&&(mousex<=high.dest.x+high.dest.w)&&(mousey>=high.dest.y)&&(mousey<=high.dest.y+high.dest.h)) {
-        cout << "high Clicked" << endl ;
-        // Open The highscoe Page
-      }
-    }*/
-  }
 }
+  break;
+  case SDL_TEXTINPUT:
+  playername+=e.text.text;
+	cout<<"x1";
+  break;
+}
+}
+if ( playername.size() ) {
+	SDL_Surface* text_surf = TTF_RenderText_Solid(font, playername.c_str(), foreground);
+	text = SDL_CreateTextureFromSurface(ren_lm, text_surf);
+
+	dest.x = 320 - (text_surf->w / 2.0f);
+	dest.y = 240;
+	dest.w = text_surf->w;
+	dest.h = text_surf->h;
+	SDL_RenderCopy(ren_lm, text, NULL, &dest);
+
+	SDL_DestroyTexture(text);
+	SDL_FreeSurface(text_surf);
+}
+
+// Update window
+SDL_RenderPresent( ren_lm );
 }
 void lastmenu(){
+	font = TTF_OpenFont("data/GTA.ttf", 72);
+	if ( !font ) {
+		cout << "Error loading font: " << TTF_GetError() << endl;
+		return;
+	}
+	SDL_StartTextInput();
   running_lm = true ;
-  //SDL_Window* win ;
   ren_lm = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-  //SDL_CreateWindowAndRenderer(350,600,0,&win,&ren_lm);
   SDL_Rect rect_fm ;
   rect_fm.x = 0 ;
   rect_fm.y = 0 ;
@@ -73,15 +97,7 @@ void lastmenu(){
   firstmenu_lm.setDest(20,415,70,70);
   firstmenu_lm.setSource(0,0,1500,1500);
   firstmenu_lm.setImage("data/play.png",ren_lm) ;
-  /* high
-  high.setDest(260,415,70,70);
-  high.setSource(0,0,1500,1500);
-  high.setImage("data/playagain_lm.png",ren_lm) ;
-  // ball
-  ball.setDest(100,490,70,70);
-  ball.setSource(0,0,1500,1500);
-  ball.setImage("data/playagain_lm.png",ren_lm) ;
-*/
+
   float hei = 0 ;
   bool down = true ;
   float sh = 2.2 ;
@@ -101,31 +117,9 @@ void lastmenu(){
     SDL_RenderCopyEx(ren_lm,playagain_lm.tex,&playagain_lm.src,&playagain_lm.dest,0,NULL,SDL_FLIP_NONE);
     SDL_RenderCopyEx(ren_lm,highscore_lm.tex,&highscore_lm.src,&highscore_lm.dest,0+rot,NULL,SDL_FLIP_NONE);
     SDL_RenderCopyEx(ren_lm,firstmenu_lm.tex,&firstmenu_lm.src,&firstmenu_lm.dest,0+rot,NULL,SDL_FLIP_NONE);
-    //SDL_RenderCopyEx(ren_lm,high.tex,&high.src,&high.dest,0+rot,NULL,SDL_FLIP_NONE);
-    //SDL_RenderCopyEx(ren_lm,ball.tex,&ball.src,&ball.dest,0+rot,NULL,SDL_FLIP_NONE);
 
     rot += 2 ;
-    if (down) {
-      WriteMessage("Swipe Brick Breaker",18,10+hei,155,0,0,35,ren_lm);
-    } else {
-      WriteMessage("Swipe Brick Breaker",18,10+hei,142,0,56,35,ren_lm);
-    }
-    pos = 10 + hei ;
-    if (down) {
-      if (pos >= 170) {
-        down = false ;
-        hei -= sh ;
-      } else {
-        hei += sh ;
-      }
-    } else {
-      if (pos <= 10) {
-        down = true ;
-        hei += sh ;
-      } else {
-        hei -= sh ;
-      }
-    }
+
     framecount++ ;
     int timerFPS = SDL_GetTicks() - lastframe ;
     if (timerFPS < (1000/10)) {
